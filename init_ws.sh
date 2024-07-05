e66e7b0631fdb7e751c744e777653b011f1db36c
LiuYuha0 <liuyuhao@hust.edu.cn>
2024-07-05 11:32:49 +0800

    add shell

---
+++ init_ws.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+printf "${YELLOW} WARNING! You don't need to run this script if both ROS1/2 workspaces have been initialized before.${NC}\n"
+read -p "Proceed? (y/n)" proceed
+if [[ ${proceed} != "y" ]]; then
+    exit
+fi
+
+cd /midea_robot/ros1_ws
+if [[ ! -d src ]]; then
+    mkdir src
+fi
+source /opt/ros/noetic/setup.bash
+catkin_make
+chmod a+rw -R /midea_robot/ros1_ws
+
+cd /midea_robot/ros2_ws
+if [[ ! -d src ]]; then
+    mkdir src
+fi
+source /opt/ros/humble/setup.bash
+colcon build
+chmod a+rw -R /midea_robot/ros2_ws
